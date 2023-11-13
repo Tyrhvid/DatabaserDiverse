069ad2e50b0e68cfce1c8a5edece874e026a7b6c
Tyrhvid <143458155+Tyrhvid@users.noreply.github.com>
2023-11-13 13:45:17 +0100

    Update TyrIntro.sql

---
+++ TyrIntro.sql
@@ -0,0 +1,25 @@
+--CREATE DATABASE KlassenDb;
+
+USE KlassenDb;
+
+--CREATE TABLE KlassenTbl(
+--Id int Primary Key,
+--Förnamn nvarchar(max) not null,
+--Efternamn nvarchar(max) not null,
+--Ålder int,
+--Hemort nvarchar(max) not null
+--)
+
+--INSERT INTO KlassenTbl VALUES (1, 'Tyr', 'Hvidfeldt', 37, 'Göteborg')
+--INSERT INTO KlassenTbl VALUES (2, 'Gabriel', 'Raimondo', 31, 'Skövde')
+--INSERT INTO KlassenTbl VALUES (3, 'Jesper', 'Whendin', 29, 'Kungsbacka')
+--INSERT INTO KLassenTbl VALUES (4, 'Joseph', 'Rashid-Maalouf', 30, 'Göteborg')
+--INSERT INTO KlassenTbl VALUES (5, 'Mikael', 'Björnänger', 35, 'Göteborg')
+
+
+
+--SELECT * FROM KlassenTbl;
+
+--SELECT * FROM KlassenTbl WHERE Namn LIKE '%a%';
+
+
